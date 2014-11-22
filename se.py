fe6a9476b6a5977b8664781682dc4fe85335a108
Matej Stuchlik <mstuchli@redhat.com>
2014-11-22 21:07:58 +0100

    [se.py] When spec file is not specified, look in pwd

--- se.py
+++ se.py
@@ -3,15 +3,18 @@
 """speck -- rpm spec modification tool
 
 Usage:
-    se.py <spec> patch add <file>
-    se.py <spec> patch disable <number>
-    se.py <spec> patch enable <number>
-    se.py <spec> patch list
+    se.py [spec] patch add <file>
+    se.py [spec] patch disable <number>
+    se.py [spec] patch enable <number>
+    se.py [spec] patch list
 
 Options:
     -h --help   Show this screen
     -d --debug  Print debug information
 """
+
+from glob import glob
+import sys
 
 from docopt import docopt
 from speck import parser
@@ -19,7 +22,14 @@ from speck import parser
 if __name__ == '__main__':
     arguments = docopt(__doc__)
 
-    parser.parse(arguments['<spec>'])
+    if not arguments['spec']:
+        specs = glob('./*.spec')
+        if len(specs) > 1:
+            print "Multiple spec file in current directory, please specify one"
+            sys.exit(1)
+        arguments['spec'] = specs[0]
+
+    parser.parse(arguments['spec'])
 
     if arguments['patch']:
         if arguments['add']:
