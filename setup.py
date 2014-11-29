86ab404e75030d6ec143c71135e44955e953d641
Matej Stuchlik <mstuchli@redhat.com>
2014-11-29 21:52:48 +0100

    [setup.py] Add proper requires

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setup(name='speck',
       include_package_data=True,
       py_modules=['speck'],
       test_suite="tests",
+      install_requires=[
+          'Click',
+          'Tabulate',
+          ],
       entry_points='''
           [console_scripts]
           speck=speck:cli
