6d0aaaf91f910c96978e7800091c10947f10d6e2
Matej Stuchlik <mstuchli@redhat.com>
2014-12-24 19:51:35 +0100

    Allow specifying more details when adding patch

--- speck/actions/__init__.py
+++ speck/actions/__init__.py
@@ -3,18 +3,19 @@ from speck.objects import Patch
 
 
 @spec.register_action(spec.add_patch, "generic")
-def add_patch(spec, patch_file):
+def add_patch(spec, patch_file, number=None, source_line_no=None,
+              apply_line_no=None):
     if spec.patches:
         last_patch = sorted(spec.patches, key=lambda x: x.source_line_no)[-1]
-        new_source_line = last_patch.source_line_no + 1
-        new_apply_line = last_patch.applied_line_no + 1
-        new_patch_number = last_patch.number + 1
+        new_source_line = source_line_no or (last_patch.source_line_no + 1)
+        new_apply_line = apply_line_no or (last_patch.applied_line_no + 1)
+        new_patch_number = number or (last_patch.number + 1)
     else:
         # there can be more sources, pick the last one
         # this seem to be a common theme ^
-        new_source_line = spec.source.line_no + 1
-        new_apply_line = spec.prep.line_no + 1
-        new_patch_number = 0
+        new_source_line = source_line_no or (spec.source.line_no + 1)
+        new_apply_line = apply_line_no or (spec.prep.line_no + 1)
+        new_patch_number = number or 0
 
     with open(spec.spec_file, 'r') as s:
         lines = s.readlines()
